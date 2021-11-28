f2954f095540cd2923d2df7186930eb6fd6694c0
jordanthopps <91483280+jordanthopps@users.noreply.github.com>
2021-11-28 18:37:05 -0500

    Home Index Presentation

---
+++ MVCAddressBook/wwwroot/js/CustomCarousel.js
@@ -0,0 +1,11 @@
+﻿var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items: 1,
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: false,
+    animateIn: 'fadeIn',
+    animateOut: 'fadeOut',
+    lazyLoad: true
+});
