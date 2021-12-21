a279e2e3ffddf154e80c4414dcc9238c55f3cdc7
jordanthopps <91483280+jordanthopps@users.noreply.github.com>
2021-12-21 16:08:08 -0500

    Production Ready

--- MVCAddressBook/wwwroot/js/site.js
+++ MVCAddressBook/wwwroot/js/site.js
@@ -1,4 +1,11 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿//////*Start Custom Sidenav*//////
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "280px";
+}
 
-// Write your JavaScript code.
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+/////*End Custom Sidenav*/////
